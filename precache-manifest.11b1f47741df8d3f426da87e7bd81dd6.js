60ed11a1013a3a29f64677182cd0826ba15927de
Irvanjit Singh Gill <irvan.gill@gmail.com>
2019-06-13 04:10:13 -0400

    Updates

---
+++ precache-manifest.11b1f47741df8d3f426da87e7bd81dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768f6ab1247cfe3467ee55ead58b98f4",
+    "url": "/guca3/index.html"
+  },
+  {
+    "revision": "91b7421f30c0bc91dfc7",
+    "url": "/guca3/static/css/main.fc698d22.chunk.css"
+  },
+  {
+    "revision": "5e3d16ef355a239356e0",
+    "url": "/guca3/static/js/2.5587f723.chunk.js"
+  },
+  {
+    "revision": "91b7421f30c0bc91dfc7",
+    "url": "/guca3/static/js/main.acf1cb22.chunk.js"
+  },
+  {
+    "revision": "e66ef47696fe6e282feb",
+    "url": "/guca3/static/js/runtime~main.0f0fde5a.js"
+  },
+  {
+    "revision": "92ffab686438db440846e5e750778f0f",
+    "url": "/guca3/static/media/GURAKHAR.92ffab68.TTF"
+  }
+]);
