884a260a54eb23748ec852f68dcef14018e57daf
Irvanjit Singh <irvanjit@gmail.com>
2019-10-22 19:11:23 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7642693521a7e9cb2e3513d98d9d3b97.js"
+  "/precache-manifest.d3ebbb937922be20fa43cf7bd8e3fb7f.js"
 );
 
 self.addEventListener('message', (event) => {
